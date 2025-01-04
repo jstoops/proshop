8d6ea0d5360bf607ad7cf402a0a283583f6a9657
John Stoops <john.stoops@gmail.com>
2025-01-03 18:03:30 -1000

    Update styling to JDSCraft

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -35,7 +35,7 @@ const HomeScreen = () => {
       ) : (
         <>
           <Meta />
-          <h1>Latest Products</h1>
+          <h1 class="logo">JDS<span class="text-primary">Craft</span></h1>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
